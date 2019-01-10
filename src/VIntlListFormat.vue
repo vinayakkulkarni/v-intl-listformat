6693aef322de4f2ec93843946f459b04661a1827
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2019-01-10 13:01:37 +0530

    :rocket: Sauce

---
+++ src/VIntlListFormat.vue
@@ -0,0 +1,37 @@
+<template>
+  <span>{{ listFormat }}</span>
+</template>
+<script>
+export default {
+  name: 'VIntlListFormat',
+  props: {
+    locale: {
+      type: String,
+      default: 'en',
+      required: false,
+    },
+    payload: {
+      type: Array,
+      default() {
+        return [];
+      },
+      required: true,
+    },
+    type: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    style: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  },
+  computed: {
+    listFormat() {
+      const t = this;
+      return new Intl.ListFormat(t.locale, { type: t.type, style: t.style }).format(t.payload);
+    },
+  },
+};
