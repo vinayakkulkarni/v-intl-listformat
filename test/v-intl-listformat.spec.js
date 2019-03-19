97e04210894b89027d2be879610d8cd0921ecaf6
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2019-03-19 22:55:26 +0530

    Updated the v-intl-listformat test cases

--- test/v-intl-listformat.spec.js
+++ test/v-intl-listformat.spec.js
@@ -7,9 +7,31 @@ function getComponent(Component, propsData) {
 }
 
 describe('VIntlListFormat', () => {
-  it('has correct DOM structure', () => {
-    const vm = getComponent(VIntlListFormat);
 
-    expect(vm.$el.nodeName).toBe('SPAN');
+  it('has correctly mounted', () => {
+    const vm = getComponent(VIntlListFormat);
+    expect(vm._isMounted).toBeTruthy();
+  });
+
+  it('has correct default props', () => {
+    const vm = getComponent(VIntlListFormat);
+    expect(vm.$props).toEqual({payload: [], locale: 'en', listType: null, listStyle: null});
+  });
+
+  it('has correct custom props', () => {
+    const exampleData = {
+      payload: ['Frank', 'Christine', 'Flora', 'Harrison'],
+      locale: 'en',
+      listType: null, 
+      listStyle: null,
+    };
+    const Ctor = Vue.extend(VIntlListFormat);
+    const vm = new Ctor({ propsData: exampleData}).$mount();
+    expect(vm.$props).toEqual({payload: exampleData.payload, locale: exampleData.locale, listType: exampleData.listType, listStyle: exampleData.listStyle});
+  });
+
+  it('has correct custom component name', () => {
+    const vm = getComponent(VIntlListFormat);
+    expect(vm.$options.name).toEqual('VIntlListFormat');
   });
 });
